536bde1a712b18f39d1c0d97e843801f10a08b0e
Adrian Sanchis Gallego <adrian@>
2023-05-01 10:38:59 +0200

    chore: add data

---
+++ src/data/index.ts
@@ -0,0 +1,18 @@
+import cereal from './cereal.json'
+import dairy from './dairy.json'
+import drink from './drink.json'
+import fruit from './fruit.json'
+import nut from './nut.json'
+import other from './other.json'
+import vegetables from './vegetables.json'
+
+export {
+    cereal,
+    dairy,
+    drink,
+    fruit,
+    nut,
+    other,
+    vegetables
+}
+export const all = [...cereal,dairy,drink,fruit,nut,other,vegetables]
