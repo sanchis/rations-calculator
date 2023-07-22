60e0291ae2bd971b4a0a23834d2775f40b666723
Adrian Sanchis Gallego <adrian@>
2023-07-22 23:32:26 +0200

    add new config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
   integrations: [react(), tailwind({
     config: {
       applyBaseStyles: false
