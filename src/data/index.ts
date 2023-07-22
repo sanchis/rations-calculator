3d6779008b5ffbd4800503aa121b4681a45f3bbc
Adrian Sanchis Gallego <adrian@>
2023-07-22 13:21:49 +0200

    feat: add new data

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,18 +1,36 @@
-import cereal from './cereal.json'
-import dairy from './dairy.json'
-import drink from './drink.json'
-import fruit from './fruit.json'
-import nut from './nut.json'
-import other from './other.json'
-import vegetables from './vegetables.json'
+// import cereal from './cereal.json'
+// import dairy from './dairy.json'
+// import drink from './drink.json'
+// import fruit from './fruit.json'
+// import nut from './nut.json'
+// import other from './other.json'
+// import vegetables from './vegetables.json'
 
+import dairy from './_dairy.json'
+import fat from './_fat.json'
+import flour from './_flour.json'
+import fruit from './_fruit.json'
+import protein from './_protein.json'
+import vegetables from './_vegetables.json'
 export {
-    cereal,
+    fat,
     dairy,
-    drink,
+    flour,
     fruit,
-    nut,
-    other,
+    protein,
     vegetables
 }
-export const all = [...cereal,dairy,drink,fruit,nut,other,vegetables]
+export interface FoodCategory {
+    id: string,
+    name: string
+    ration: number
+}
+
+// export enum CategoryType {
+//     CEREAL,
+//     DAIRY,
+//     DRINK,
+//     FRUIT,
+//     NUT,
+//     VEGETABLES
+// }
